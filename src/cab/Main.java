3217c7c5bac3f3bdec5b543ae54dc64b5533bd4e
Dmitry <dmitry.osichkin@gmail.com>
2018-10-23 21:09:19 +0300

    Initial commit

---
+++ src/cab/Main.java
@@ -0,0 +1,35 @@
+package cab;
+
+
+import javafx.application.Application;
+import javafx.beans.binding.Binding;
+import javafx.beans.binding.Bindings;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+   launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane root = FXMLLoader.load(getClass().getResource("/cab/Main.fxml"));
+
+
+        Scene scene = new Scene(root,500,300 );
+        primaryStage.setTitle("Bulls and Cows");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
+
